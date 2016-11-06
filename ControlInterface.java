5f61bac004589921f0df76c8f060fb5d321620f4
Handsome Yang <3300833570@qq.com>
2016-11-06 23:14:00 +0800

    Add files via upload

--- ControlInterface.java
+++ ControlInterface.java
@@ -3,7 +3,6 @@ package Service;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
 public interface ControlInterface extends Remote {
 
     public void controlKeyPress(int keycode) throws RemoteException;    // 执行远程主机键盘指定按键的按下动作
